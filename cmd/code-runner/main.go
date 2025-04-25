0e81e8300e41f50a8e5fc6a7c4864c29c86afed4
erfanghorbani <erfan23g@icloud.com>
2025-04-25 12:23:08 +0330

    Changed code-runner

--- cmd/code-runner/main.go
+++ cmd/code-runner/main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"net/http"
 	"os"
 	"os/exec"
 	"strings"
@@ -59,109 +57,9 @@ func RunGoCode(code string, input string, expectedOutput string, timeLimit int, 
         return "Time limit exceeded"
     }
 }
-type SubmissionRun struct {
-	ID 		   uint   `json:"id"`
-	Code           string `json:"code"`
-	Input          string `json:"input"`
-	ExpectedOutput string `json:"expectedOutput"`
-	TimeLimit      int    `json:"timeLimit"`
-	MemoryLimit    int    `json:"memoryLimit"`
-}
 
-type SubmissionRunResp struct {
-	Submissions []SubmissionRun `json:"submissions"`
-}
-
-type SubmissionResult struct {
-	ID 		   uint   `json:"id"`
-	Status         int `json:"status"`
-}
-
-// SendResultToServer sends the result of the code execution to the main server
-func SendResultToServer(result SubmissionResult) {
-	// The server URL to send the result
-	serverURL := "http://localhost:8000/runner/result" // Change this to your server's actual URL
-
-	// Convert the result to JSON
-	resultJSON, err := json.Marshal(result)
-	if err != nil {
-		fmt.Printf("Error marshaling result: %v\n", err)
-		return
-	}
-
-	// Send a POST request to the server
-	resp, err := http.Post(serverURL, "application/json", bytes.NewBuffer(resultJSON))
-	if err != nil {
-		fmt.Printf("Error sending result: %v\n", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	// Print the server's response (optional)
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("Error reading server response: %v\n", err)
-		return
-	}
-	fmt.Printf("Server response: %s\n", body)
-}
 
 func PollForCode() {
-	for {
-		// Make a request to the main program's /get-code endpoint
-		resp, err := http.Get("http://localhost:8000/runner/get")
-		if err != nil {
-			fmt.Printf("Error fetching code: %v\n", err)
-			time.Sleep(2 * time.Minute) // Wait before retrying
-			continue
-		}
-
-		// Decode the response into CodeRequest struct
-		var srr SubmissionRunResp
-		if err := json.NewDecoder(resp.Body).Decode(&srr); err != nil {
-			fmt.Printf("Error decoding response: %v\n", err)
-			time.Sleep(2 * time.Minute) // Wait before retrying
-			continue
-		}
-
-		// Run the Go code
-		for _, codeRequest := range srr.Submissions {
-			fmt.Printf("Running code: %s\n", codeRequest.Code)
-			fmt.Printf("Input: %s\n", codeRequest.Input)
-			fmt.Printf("Expected Output: %s\n", codeRequest.ExpectedOutput)
-			fmt.Printf("Time Limit: %d\n", codeRequest.TimeLimit)
-			go func(codeRequest SubmissionRun) {
-				// Run the code with the provided input and expected output
-				result := RunGoCode(codeRequest.Code, codeRequest.Input, codeRequest.ExpectedOutput, codeRequest.TimeLimit, (codeRequest.MemoryLimit))
-				// result := "OK"
-				var status int
-				switch result {
-				case "OK":
-					status = 3
-				case "Wrong output":
-					status = 4
-				case "Time limit exceeded":
-					status = 5
-				case "Compile error":
-					status = 6
-				case "Memory limit exceeded":
-					status = 7
-				case "Runtime error":
-					status = 8
-				}
-
-				// Send the result to the server
-				submissionResult := SubmissionResult{
-					ID: 		  codeRequest.ID,
-					Status:         status,
-				}
-				SendResultToServer(submissionResult)
-			}(codeRequest)
-		}
-
-		// Wait for the next cycle (2 minutes)
-		time.Sleep(2 * time.Minute)
-	}
 }
 
 func main() {
